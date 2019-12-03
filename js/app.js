860e25029e73f0f918880949b72c770982bdd431
Jimmy <bruno_pier@hotmail.com>
2019-12-03 00:35:08 -0300

    Datos primitivos

---
+++ js/app.js
@@ -0,0 +1,33 @@
+/*
+
+**** DATOS PRIMITIVOS
+var cadena ="Ada Lovelace";
+var pi = 3.14;
+var bool=true/false;
+var nodefinida=undefined;
+var nula=null;
+
+
+**** objetos nativos:
+** No dependen del navegador.
+** No es recomendable usar los obj. nativos( String,Number,Boolean)
+para definir datos, es mejor usar los datos primitivos.
+- String
+- Number
+- Boolean
+- Data
+- Math
+- RegExp (Expresiones regulares)
+- Array
+- Function
+- Object
+
+**** Objetos de ALTO Nivel: Dependen del navegador.
+- Window > Corresponde al modelo de objetos de "Navegador"
+- Screen > Corresponde al modelo de objetos de "Navegador"
+- Navigator > Corresponde al modelo de objetos de "Navegador"
+- Location  > Corresponde al modelo de objetos de "Navegador"
+- History  > Corresponde al modelo de objetos de "Navegador"
+- Document > Corresponde al modelo de objetos de "documento" > Para
+documentos html dentro del documento.
+*/
