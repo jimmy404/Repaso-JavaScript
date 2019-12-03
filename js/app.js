37297a519ae5f488f5f775cfcd19c6ecb8049e88
Jimmy <bruno_pier@hotmail.com>
2019-12-03 00:55:15 -0300

    Funciones

--- js/app.js
+++ js/app.js
@@ -489,5 +489,32 @@ crearProducto100("Corrector");
 crearProductoCamisa(75);
 
 
+**** FUNCIONES // CONTEXTO
+function crearFunciones(){
+
+var arr=[];
+var numero = 1;
+
+for(var numero=1;numero<=3; numero ++){
+arr.push(
+    (function(numero){
+        return  function(){
+   console.log(numero);
+ }
+          })(numero)
+   
+        );
+    }
+
+return arr;
+
+}
+var funciones=crearFunciones();
+
+funciones[0]();
+funciones[1]();
+funciones[2]();
+
+
 
 */
