b1964bd088c68a021922e2e197d458806c91922b
Jimmy <bruno_pier@hotmail.com>
2019-12-03 01:07:57 -0300

    Var-Const-Let

---
+++ js/app.js
@@ -0,0 +1,32 @@
+/* **** Creando variables
+
+**** variables con var
+
+var aprendiendo = 'JavaScript';
+
+
+**** Variables con const
+
+const aprendiendo = 'JavaScript';
+aprendiendo = true;
+
+(deben tener valor de inicio> sino error)
+valor constante> nunca cambia.
+
+**** Let
+
+let aprendiendo = 'JavaScript';
+aprendiendo = true; 
+console.log (aprendiendo);
+
+
+
+
+
+
+
+
+
+*/
+
+
