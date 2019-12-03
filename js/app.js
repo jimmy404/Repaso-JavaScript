2fcbb67a6bdc7d0ed3eecae0c980c2b0cbe49b2b
Jimmy <bruno_pier@hotmail.com>
2019-12-03 01:15:13 -0300

    Scope

--- js/app.js
+++ js/app.js
@@ -16,17 +16,29 @@ valor constante> nunca cambia.
 **** Let
 
 let aprendiendo = 'JavaScript';
-aprendiendo = true; 
+aprendiendo = true;
 console.log (aprendiendo);
 
+**** Scope (Alcance de los valores> Var= se mezclan // Let-Cons dentro de llaves NO)
+
+JavaScript tiene un alcance de función: cada función crea un nuevo alcance. El alcance determina la accesibilidad (visibilidad) de estas variables.
+Las variables definidas dentro de una función no son accesibles (visibles) desde fuera de la función.
 
 
+var musica = 'Rock';
 
+if (musica){
+    var musica = 'Grunge';
+    console.log('Dentro del if :', musica);
+}
+console.log('Fuera del if :', musica);
 
+let musica = 'Rock';
 
-
-
-
-*/
+if(musica){
+    let musica = 'Grunge';
+    console.log('Dentro del if :', musica);
+}
+console.log('Fuera del if :', musica);*/
 
 
